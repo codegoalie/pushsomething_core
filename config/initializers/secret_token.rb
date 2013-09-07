95fdde73721dd9e72f33c01aaac74383f9095d45
Chris Marshall <chris@chrismar035.com>
2013-09-07 16:46:41 -0400

    Initial Commit; blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PushRails::Application.config.secret_token = '1b146a1676486e21e9f408201715e77de13e0540baca6f67e82df8c8540a8cc251971744ae1d56bda23ad75a9d645baa5469fc6a78275d42be5a00da87f23faf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PushRails::Application.config.session_store :cookie_store, key: '_push_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PushRails::Application.config.session_store :active_record_store

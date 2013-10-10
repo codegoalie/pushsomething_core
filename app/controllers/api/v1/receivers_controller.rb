7fda3155d23634c21d2f13737df6b6f451097b96
Chris Marshall <chris@chrismar035.com>
2013-10-10 09:05:45 -0400

    Verify JWT before creating receivers

--- app/controllers/api/v1/receivers_controller.rb
+++ app/controllers/api/v1/receivers_controller.rb
@@ -1,5 +1,20 @@
 class Api::V1::ReceiversController < ActionController::Base
 
+  before_filter :verify_jwt
+
   def create
   end
+
+  private
+
+    def verify_jwt
+      jwt = JWT.decode(params['jwt'], ENV['GOOGLE_SECRET'], false)
+
+      if jwt['iss'] != 'accounts.google.com' &&
+         jwt['aud'] != ENV['GOOGLE_ID']
+        render 'auth_error'
+      end
+
+      @user_email = jwt['email']
+    end
 end

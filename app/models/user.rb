4a268e8faa72d09d2a1dd1dab622368925cf2fab
Chris Marshall <chris@chrismar035.com>
2013-10-10 09:04:15 -0400

    Define Receiver model and associate with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
+  has_many :receivers
+
   attr_accessible :name, :email, :password, :remember_me
 
   def self.find_for_google_oauth2(access_token, signed_in_resource = nil)

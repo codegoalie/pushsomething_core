d14b8a645991803679bdccacc7bd9d6567b2658c
Chris Marshall <chris@chrismar035.com>
2013-10-25 19:57:48 -0400

    Verify require params exist to create a reciever

--- app/controllers/api/v1/receivers_controller.rb
+++ app/controllers/api/v1/receivers_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ReceiversController < ActionController::Base
 
+  before_filter :verify_params
   before_filter :verify_jwt
 
   def create
@@ -22,5 +23,11 @@ class Api::V1::ReceiversController < ActionController::Base
       end
 
       @user_email = jwt['email']
+    end
+
+    def verify_params
+      unless params['jwt'] && params['uid'] && params['gcm_id']
+        render status: :bad_request, json: {}
+      end
     end
 end

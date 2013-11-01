f602fb2b0491bacc17d37073a6a54ba4ec032fec
Chris Marshall <chris@chrismar035.com>
2013-10-31 23:23:39 -0400

    Define basic factories for users and receivers

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Chris Marshall'
+    sequence(:email) {|n| "person#{n}@example.com" }
+  end
+
+  factory :receiver do
+    uid '12345'
+    user
+    gcm_id '54321'
+    sequence(:nickname) {|n| "Recever #{n}" }
+  end
+end

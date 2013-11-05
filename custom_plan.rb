8ce1e7ea0bce69de74cc484b52d9381ca5b3d09a
Chris Marshall <chris@chrismar035.com>
2013-11-04 22:59:45 -0500

    Prevent Zeus from unrandomizing Rspec

--- custom_plan.rb
+++ custom_plan.rb
@@ -5,7 +5,10 @@ class CustomPlan < Zeus::Rails
   # def my_custom_command
   #  # see https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
   # end
-
+  def rspec
+    RSpec.configuration.seed = rand 1..10_000
+    exit RSpec::Core::Runner.run(ARGV)
+  end
 end
 
 Zeus.plan = CustomPlan.new

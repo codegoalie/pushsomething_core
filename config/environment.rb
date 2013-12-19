11e155b2bb7a3ea0fdb734063c7dc4ea1b6538df
Chris Marshall <chris@chrismar035.com>
2013-12-18 21:13:00 -0500

    Only log info and above

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,8 @@ if Rails.env.development?
                         debug: true,
                         local: :true)
 elsif Rails.env.production?
-  Rails.logger = Le.new('4988322a-eca9-47cd-b82d-a903e90b9a74')
+  Rails.logger = Le.new('4988322a-eca9-47cd-b82d-a903e90b9a74',
+                        log_level: Logger::INFO)
 end
 
 # Initialize the rails application

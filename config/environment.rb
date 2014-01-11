53298d1ababed60cd14fa3c42f54d7c685ff34ec
Chris Marshall <chris@chrismar035.com>
2014-01-11 11:26:06 -0500

    Only use LogEntries in production

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # LogEntries configuration
-if Rails.env.development?
-  Rails.logger = Le.new('4988322a-eca9-47cd-b82d-a903e90b9a74',
-                        debug: true,
-                        local: :true)
-elsif Rails.env.production?
+if Rails.env.production?
   Rails.logger = Le.new('4988322a-eca9-47cd-b82d-a903e90b9a74',
                         log_level: Logger::INFO)
 end

0b1190cc4c172e888fadb170e7ab2823dc20dcb8
Chris Marshall <chris@chrismar035.com>
2014-01-31 22:30:13 -0500

    Cookies are now encryptes based on the key base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 PushRails::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
+PushRails::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY_BASE']

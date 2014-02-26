05274b42636d6997c54fb2188df57b7b3fbbefcd
Chris Marshall <chris@chrismar035.com>
2014-02-25 23:20:44 -0500

    Use secure params to create Services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,9 @@
 class ServicesController < ApplicationController
   load_and_authorize_resource
 
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
+  def index; end
+  def show; end
+  def edit; end
 
   def update
     if @service.update_attributes(service_params)
@@ -23,6 +18,8 @@ class ServicesController < ApplicationController
   end
 
   def create
+    @service = Service.new(service_params)
+
     if @service.save
       redirect_to services_path
     else

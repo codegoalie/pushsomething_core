b83feac00c606943e38fe591078acb9493384d6b
Chris Marshall <chris@chrismar035.com>
2014-07-25 13:36:30 -0400

    Push sidekiq errors to airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,9 @@
 Airbrake.configure do |config|
   config.api_key = 'c5abef747faf83ebe799e804c215ed8b'
+
+# Airbrake error catching for Sidekiq workers
+Sidekiq.configure_server do |config|
+  config.error_handlers << Proc.new do |ex, ctx_hash|
+    Airbrake.notify(ex, ctx_hash)
+  end
 end
